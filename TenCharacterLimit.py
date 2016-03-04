3b8a1d4ddd47be3e0f3662d7a03e08ad9a9383c0
Darlene Fung <Darlene@Darlenes-MacBook-Pro.local>
2016-03-04 11:33:25 -0500

    working program with embellishments

--- TenCharacterLimit.py
+++ TenCharacterLimit.py
@@ -1,22 +1,23 @@
 # Darlene Fung
-#3/2/16
+# 3/2/16
 
-name = raw_input("What is your name? ") # asks the user for input
+name = raw_input("What is your name? ") 															# asks the user for input
 
-inputFile = open("input.txt", "w") # creates inputFile
-outputFile = open("output.txt", "w")
+inputFile = open("input.txt", "w") 																	# creates input 
+outputFile = open("output.txt", "w")																# creates output file 
 
-answer = raw_input("Would you like to add more text to the file? ")
-while answer != "no":
-	inputFile = open("input.txt", "a")
-	lengthAnswer = answer[:10]
-	outputFile.write(lengthAnswer + "\n")
-	answer = raw_input("Would you like to add more text to the file? ")
+answer = raw_input("Would you like to add more text to the file? Type 'no' to exit program. ")		# asks the user to enter text, or to exit
+
+while answer != "no":																				# when user is entering information and doesn't want to end the program 
+	inputFile = open("input.txt", "a")																# opens input file in append so information can be added
+	lengthAnswer = answer[:10]																		# reads first ten characters of the user's answer they typed in
+	outputFile.write(lengthAnswer + "\n")															# prints the ten characters in the out put file and adds an enter for the next line of text
+	answer = raw_input("Would you like to add more text to the file? Type 'no' to exit program. ")	# asks the user to enter more information, or to exit 
 	
 else: 
-	print("{}, check out the output file! (output.txt)".format(name))
+	print("{}, check out the output file! (output.txt)".format(name)) 								# if the user enters "no", it says goodbye
 	
-inputFile.close()
-outputFile.close()
+inputFile.close()																					# closes input File
+outputFile.close()																					# closes output File
 
 	
